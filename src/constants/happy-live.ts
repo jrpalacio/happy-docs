9ed8c6bbd8b93901627b3a02d12e03b5a7728613
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-05-13 16:09:39 -0600

    feat(web): agregar constantes de happy live

---
+++ src/constants/happy-live.ts
@@ -0,0 +1,12 @@
+interface link {
+    href: string;
+    urlIcon: string;
+    title: string;
+}
+
+export const shopLink: link = {
+    href: 'https://s.happylive.mx:1035/h/iCommerce/templates/Happylive/CarritoCompras/index.jsp?ID=2270',
+    urlIcon: '../assets/svg/building-store.svg',
+    title: "Tienda en línea de Happy Live"
+}
+    
