51aad575feef96c1b45eedf09e3a754f2f7521ba
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-05-18 12:03:53 -0600

    feat(dosis): agregar componente vue TimeHeader

---
+++ src/components/TimeHeader.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { LANG } from '../constants/date_config'
+
+const time = ref('')
+
+const getCurrentDateTime = () => {
+  const now = new Date()
+  time.value = now.toLocaleTimeString(LANG)
+}
+
+onMounted(() => {
+  getCurrentDateTime()
+  setInterval(getCurrentDateTime, 1000)
+})
+</script>
+
+<template>
+  <header class="text-3xl font-bold text-neutral-100 text-center py-4" >
+    <h1>{{ time }}</h1>
+  </header>
+</template>
+
