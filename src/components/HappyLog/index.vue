fa160a282f4d781458dae984c2133b4236335882
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-05-23 21:53:41 -0600

    feat(web): usar lista de productos

--- src/components/HappyLog/index.vue
+++ src/components/HappyLog/index.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { storeToRefs } from 'pinia'
+import IconMinus from './icons/IconMinus.vue'
+import IconPlus from './icons/IconPlus.vue'
 import IconDropletPlus from './icons/IconDropletPlus.vue'
+import { PRODUCTS } from '../../constants/happy-live'
 import HappyDropper from '../../assets/images/happy-dropper.webp'
+
 import ModalContent from './ModalContent.vue'
-
-
 import CalendarControl from './CalendarControl.vue'
 
 import { useDropStore } from '../../stores/drop.ts'
 
 const showModal = ref(false)
+const products = ref(PRODUCTS)
+
+const selectedDropper = ref('')
+const dateModal = ref(new Date().toISOString().split('T')[0])
+const timeModal = ref(new Date().toISOString().split('T')[1])
 
 const dropStore = useDropStore()
 const { addDrop } = dropStore
@@ -61,28 +68,33 @@ function agregarDrop() {
     <template #header>Agregar dosis</template>
     <template #body>
       <div class="flex flex-col space-y-4">
-          <label class="flex flex-col">
-            <span class="text-sm text-white font-semibold">Producto</span>
-            <div class="border border-neutral-600 p-2 rounded-lg">
-              <select name="droppers" v-model="selectedDropper" class="w-full">
-              <option value="" disabled>Elige tu producto</option>
-              <option v-for="product in products" :value="product.name" :key="product.id">
-                {{ product.name }}
-              </option>
-            </select>
-            </div>
-            
-          </label>    
-          <label class="flex flex-col">
-            <span class="text-sm text-white font-semibold">Fecha</span>
-            <input type="date" :value="dateModal" disabled class="border border-neutral-600 p-2 rounded-lg" />
-          </label>
-          <label class="flex flex-col">
-            <span class="text-sm text-white font-semibold">Hora</span>
-            <input class="time border border-neutral-600 p-2 rounded-lg" type="time" v-model="timeModal"   />
-          </label>
-          <button @click="agregarDrop" class="text-white p-2 bg-orange-500 rounded-xl">Agregar</button>
+        <label class="flex flex-col gap-1">
+          <span class="text-sm text-white font-semibold">Producto</span>
+          <div class="border border-neutral-600 p-2 rounded-lg">
+            <select name="droppers" v-model="selectedDropper" class=" text-[#ec7357] font-semibold w-full">
+            <option value="" disabled>Elige tu producto</option>
+            <option v-for="product in products" :value="product.name" :key="product.id">
+              {{ product.name }}
+            </option>
+          </select>
+          </div>
+          
+        </label>    
+        <label class="flex flex-col gap-1">
+          <span class="text-sm text-white font-semibold">Fecha</span>
+          <input type="date" :value="dateModal" disabled class="text-neutral-300 font-semibold border border-neutral-600 p-2 rounded-lg bg-neutral-700" />
+        </label>
+        <label class="flex flex-col gap-1">
+          <span class="text-sm text-white font-semibold">Hora</span>
+          <input type="time" v-model="timeModal"  class="time border border-neutral-600 p-2 rounded-lg text-[#ec7357] font-semibold"/>
+        </label>
+        <div class="flex items-center gap-2">
+          <IconMinus/>  
+          <span>1</span>
+          <IconPlus/>
         </div>
+        <button @click="agregarDrop" class="text-white p-2 bg-orange-500 rounded-xl">Agregar</button>
+      </div>
     </template>
   </ModalContent>
 </template>
