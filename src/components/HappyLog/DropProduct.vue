88ba36184c239c2634848bf523c0c30d48a5e097
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-05-27 22:00:11 -0600

    feat(web): agregar DropProduct

---
+++ src/components/HappyLog/DropProduct.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { PRODUCTS } from '../../constants/happy-live'
+
+const products = ref(PRODUCTS)
+const selectedDropper = ref('')
+
+</script>
+
+<template>
+  <label class="flex flex-col gap-1">
+    <span class="text-sm text-white font-semibold">Producto</span>
+    <select name="droppers" v-model="selectedDropper" class="w-full input-primary">
+        <option value="" disabled>Elige tu producto</option>
+        <option v-for="product in products" :value="product.name" :key="product.id">
+          {{ product.name }}
+        </option>
+      </select>
+  </label> 
+</template>
