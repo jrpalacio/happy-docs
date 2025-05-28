a3a0cac9920ef33a58d3a8264218600de08c8162
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-05-27 21:59:51 -0600

    feat(web): agregar DropDate

---
+++ src/components/HappyLog/DropDate.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { format } from "@formkit/tempo"
+import { onMounted, ref, watch } from 'vue'
+import { useDateStore } from '../../stores/date.ts'
+
+const dateStore = useDateStore()
+const { getDatePayload, setTime } = dateStore
+
+const currentTime = ref('')
+const currentDate = ref('')
+
+onMounted(() => {
+  const t = new Date()
+  currentDate.value = getDatePayload()?.shortFormat || format(t, 'yyyy-MM-dd', 'es')
+  currentTime.value = format(t, 'HH:mm', 'es')
+  setTime(currentTime.value)
+})
+
+watch(currentTime, (newTime) => {
+  setTime(newTime)
+})
+
+</script>
+<template>
+  <!-- Fecha -->
+  <label class="flex flex-col gap-1">
+    <span class="text-sm text-white font-semibold">Fecha</span>
+    <input type="date" :value="currentDate" class="input-primary bg-neutral-700" disabled />
+  </label>
+  <!-- Hora -->
+  <label class="flex flex-col gap-1">
+    <span class="text-sm text-white font-semibold">Hora</span>
+    <input type="time" class="input-primary" v-model="currentTime"/>
+  </label>
+</template>
