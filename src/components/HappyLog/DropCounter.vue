c1600259c82030b95ade5845b424b936e129af09
José Reyes Palacio <jrpalacio.dev@gmail.com>
2025-06-22 17:40:50 -0600

    refactor(DropCounter): actualizar la lógica de porciones para utilizar useProductStore, mejorando la gestión de datos y la interfaz de usuario

--- src/components/HappyLog/DropCounter.vue
+++ src/components/HappyLog/DropCounter.vue
@@ -5,40 +5,43 @@ import IconPlus from './icons/IconPlus.vue'
 import { onMounted, ref } from 'vue'
 import type { Ref } from 'vue'
 
-import { useDropStore } from '../../stores'
-const dropStore = useDropStore()
-const { getPortion, setPortion } = dropStore  
+import { useProductStore } from '../../stores'
+const productStore = useProductStore()
+const { getProductPortion, setProductPortion } = productStore  
 
 const portion: Ref<number|null> = ref(null)
 
 function incrementPortion() {
   if (portion.value !== null) {
     portion.value += 1
-    setPortion(portion.value)
+    setProductPortion(portion.value)
   }
 }
 
 function decrementPortion() {
   if (portion.value !== null && portion.value > 1) {
     portion.value -= 1
-    setPortion(portion.value)
+    setProductPortion(portion.value)
   }
 }
 
 onMounted(() => {
-  portion.value = getPortion()
+  portion.value = getProductPortion()
 })
 
 </script>
 
 <template>
-  <div class="flex justify-between items-center">
-    <button v-on:click="decrementPortion" type="button" class="text-white border border-neutral-600 p-2 rounded-lg bg-neutral-700">
-      <IconMinus/>
-    </button>
-    <span class="text-white font-semibold">{{ portion }}</span>
-    <button v-on:click="incrementPortion" type="button" class="text-white border border-neutral-600 p-2 rounded-lg bg-neutral-700">
-      <IconPlus/>
-    </button>
+  <div class="flex flex-col gap-1">
+    <p class="text-white font-semibold ">Porción</p>
+    <div class="flex justify-between items-center">
+      <button v-on:click="decrementPortion" type="button" class="text-white border border-neutral-600 p-2 rounded-lg bg-neutral-700">
+        <IconMinus/>
+      </button>
+      <span class="text-[#ec7357] font-semibold">{{ portion }}</span>
+      <button v-on:click="incrementPortion" type="button" class="text-white border border-neutral-600 p-2 rounded-lg bg-neutral-700">
+        <IconPlus/>
+      </button>
+    </div>
   </div>
 </template>
